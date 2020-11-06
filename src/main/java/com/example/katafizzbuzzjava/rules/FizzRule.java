9beacda0bf3ad7791a84facb005811a5596c1b69
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-06 13:31:00 +0100

    fix test for condition of fizz rule

--- src/main/java/com/example/katafizzbuzzjava/rules/FizzRule.java
+++ src/main/java/com/example/katafizzbuzzjava/rules/FizzRule.java
@@ -4,8 +4,7 @@ public class FizzRule implements RulesInterface {
 
 	@Override
 	public boolean condition(final Integer number) {
-		// TODO Auto-generated method stub
-		return false;
+		return (3 % number) == 0;
 	}
 
 	@Override
