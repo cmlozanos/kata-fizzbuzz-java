0c8d290f6b30b1afe85b9e17573fe3474a44c1e7
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-06 12:25:46 +0100

    add test for number divisible by 3

--- src/main/java/com/example/katafizzbuzzjava/KataFizzBuzzJavaApplication.java
+++ src/main/java/com/example/katafizzbuzzjava/KataFizzBuzzJavaApplication.java
@@ -5,4 +5,9 @@ public class KataFizzBuzzJavaApplication {
 	public static void main(final String[] args) {
 		System.out.println("Hello Kata");
 	}
+
+	public String transform(final Integer number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/test/java/com/example/katafizzbuzzjava/KataFizzBuzzJavaApplicationTest.java
+++ src/test/java/com/example/katafizzbuzzjava/KataFizzBuzzJavaApplicationTest.java
@@ -1,10 +1,20 @@
 package com.example.katafizzbuzzjava;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class KataFizzBuzzJavaApplicationTest {
 
 	@Test
-	public void applicationLoad() {
+	public void givenNumberDivisibleBy3WhenTransformThenShouldReturnFizz() {
+		// given
+		final KataFizzBuzzJavaApplication app = new KataFizzBuzzJavaApplication();
+		final Integer number = 3;
+
+		// when
+		final String result = app.transform(number);
+
+		// then
+		Assertions.assertEquals("Fizz", result);
 	}
 }
