8b9ac79b540132a5595fbcfc2df2b25dcf0303c2
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-09 16:36:01 +0100

    refactor KataFizzBuzz to RuleEngine

--- src/main/java/com/example/katafizzbuzzjava/KataFizzBuzzJavaApplication.java
+++ src/main/java/com/example/katafizzbuzzjava/KataFizzBuzzJavaApplication.java
@@ -1,37 +1,16 @@
 package com.example.katafizzbuzzjava;
 
+import com.example.katafizzbuzzjava.rules.RulesEngine;
+
 public class KataFizzBuzzJavaApplication {
+
+	private final RulesEngine rulesEngine = new RulesEngine();
 
 	public static void main(final String[] args) {
 		System.out.println("Hello Kata");
 	}
 
 	public String transform(final Integer number) {
-		if (null == number) {
-			throw new IllegalArgumentException();
-		}
-
-		if (this.isFizz(number)) {
-			return "Fizz";
-		}
-		if (this.isBuzz(number)) {
-			return "Buzz";
-		}
-		if (this.isFizzBuzz(number)) {
-			return "FizzBuzz";
-		}
-		return number.toString();
-	}
-
-	private boolean isFizzBuzz(final Integer number) {
-		return (15 % number) == 0;
-	}
-
-	private boolean isBuzz(final Integer number) {
-		return (5 % number) == 0;
-	}
-
-	private boolean isFizz(final Integer number) {
-		return (3 % number) == 0;
+		return this.rulesEngine.executeRules(number);
 	}
 }
--- src/main/java/com/example/katafizzbuzzjava/rules/RulesEngine.java
+++ src/main/java/com/example/katafizzbuzzjava/rules/RulesEngine.java
@@ -7,7 +7,7 @@ public class RulesEngine {
 
 	private final ArrayList<RulesInterface> rules;
 
-	RulesEngine() {
+	public RulesEngine() {
 		this.rules = new ArrayList<>();
 		this.rules.add(new FizzBuzzRule());
 		this.rules.add(new FizzRule());
