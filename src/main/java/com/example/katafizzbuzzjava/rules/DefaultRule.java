50289b525faeb48ab823dcb1662ea1cde296aade
Carlos Lozano <carlosmlozanos@gmail.com>
2020-11-09 15:46:40 +0100

    add test for default rule

---
+++ src/main/java/com/example/katafizzbuzzjava/rules/DefaultRule.java
@@ -0,0 +1,17 @@
+package com.example.katafizzbuzzjava.rules;
+
+public class DefaultRule implements RulesInterface {
+
+	@Override
+	public boolean condition(final Integer number) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public String apply() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
